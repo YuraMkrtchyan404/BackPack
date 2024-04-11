709a24046236c301ba48b48a435b69689102dac9
YuraMkrtchyan404 <yuramkrtchyan200101@gmail.com>
2024-04-12 01:51:29 +0400

    added server side zfs snapshot creation. Currently snapshotting gets triggered by any chages in /backup-pool/backup_data, which is not optimal. Need to implement grpc communication

--- agent/src/setup_snapshot.py
+++ agent/src/setup_snapshot.py
@@ -14,9 +14,10 @@ logging.basicConfig(level=logging.INFO,
                     ])
 
 #TODO We should add permission to elioctl, mount and so on during the "make" process
+#TODO something is off with absolute paths
 
 def load_config():
-    with open("/home/yura/capstone/OS_Snapshots/agent/config.toml", "r") as file:
+    with open("agent/config.toml", "r") as file:
         config = toml.load(file)
     return config
 
@@ -38,9 +39,9 @@ def destroy_snapshot(minor, max_retries=15, retry_delay=5):
     return False
 
 def get_block_device_for_folder(folder_path):
+   # print(f"folder_path {folder_path}")
     abs_folder_path = os.path.abspath(folder_path)
-
-    print(abs_folder_path)
+    #print(f"Lets check {abs_folder_path}")
     cmd = ['df', '-h', abs_folder_path]
     df_output = subprocess.run(cmd, capture_output=True, text=True)
 
@@ -69,6 +70,9 @@ def get_free_minor():
 
 def mount_snapshot(snapshot_path, mount_point):
     snapshot_dir = os.path.join(mount_point, os.path.basename(snapshot_path))
+    #print(f"mount dir {snapshot_dir}")
+    #print(f"Heree path  {snapshot_path}")
+    
     os.makedirs(snapshot_dir, exist_ok=True)
 
     mount_cmd = ['sudo', 'mount', snapshot_path, snapshot_dir]
@@ -94,10 +98,10 @@ def umount_snapshot(snapshot_path):
     return True
 
 def full_path_of_mounted_folder(mount_point, snapshot_path, folder_path):
-    abs_folder_path = os.path.abspath(folder_path)   #TODO absolute path checking
+    abs_folder_path = os.path.abspath(folder_path)
     snapshot_dir = os.path.join(mount_point, os.path.basename(snapshot_path))
     mounted_folder_path = os.path.join(snapshot_dir, abs_folder_path.lstrip('/'))
-
+   # print(mounted_folder_path)
     if os.path.exists(mounted_folder_path):
         logging.info("Full path of the mounted folder in snapshot: %s", mounted_folder_path)
         return mounted_folder_path
@@ -124,42 +128,62 @@ def backup_to_server(mounted_folder_path):
 
 def setup_snapshot():
     config = load_config()
-    folder_path = config.get("folders")[0]  #TODO multiple folder selection
-    block_device = get_block_device_for_folder(folder_path)
-    if not block_device:
-        return False
-    minor = get_free_minor()
-    if not minor:
-        return False
-    
-    cow_directory = "/var/OS_Snapshot/cow"  #TODO cow files in the same block device
-    cow_file_path =  f"{cow_directory}/cow_file{minor}"
-    
-    cmd = ['sudo', 'elioctl', 'setup-snapshot', block_device, cow_file_path, minor]
-    setup_output = subprocess.run(cmd)
-    
-    if setup_output.returncode != 0:
-        logging.error("Error occurred while creating snapshot.")
-        return False
+    folders = config.get("folders")
+    block_devices = set()  
+    folder_mapping = {}
 
-    snapshot_path = f"/dev/elastio-snap{minor}"
-    mount_point = "/var/OS_Snapshot/data"
-    
-    if not mount_snapshot(snapshot_path, mount_point):
-        destroy_snapshot(minor)
-        return False
-    
-    mounted_folder_path = full_path_of_mounted_folder(mount_point, snapshot_path, folder_path)
-    if not mounted_folder_path:
-        return False
-    
-    if not backup_to_server(mounted_folder_path):
+
+    for folder in folders:
+        block_device = get_block_device_for_folder(folder)
+        if block_device:
+            block_devices.add(block_device)
+            if block_device in folder_mapping:
+                folder_mapping[block_device].append(folder)
+            else:
+                folder_mapping[block_device] = [folder]
+
+    for block_device in block_devices:
+        minor = get_free_minor()
+        if not minor:
+            return False
+        
+        device_folders = folder_mapping.get(block_device, [])
+
+        folder_directory = os.path.dirname(os.path.abspath(device_folders[0])) if device_folders else ""
+
+        if not folder_directory:
+            folder_directory = "/var/OS_Snapshot/cow"
+            os.makedirs(folder_directory, exist_ok=True)
+
+        cow_file_path = os.path.join(folder_directory, f"cow_file{minor}")
+
+        cmd = ['sudo', 'elioctl', 'setup-snapshot', block_device, cow_file_path, minor]
+        setup_output = subprocess.run(cmd)
+        
+        if setup_output.returncode != 0:
+            logging.error("Error occurred while creating snapshot.")
+            return False
+
+        snapshot_path = f"/dev/elastio-snap{minor}"
+        mount_point = "/var/OS_Snapshot/data"
+        
+        if not mount_snapshot(snapshot_path, mount_point):
+            destroy_snapshot(minor)
+            return False
+
+        for folder in device_folders:
+            mounted_folder_path = full_path_of_mounted_folder(mount_point, snapshot_path, folder)
+            if not mounted_folder_path:
+                #umount_snapshot(snapshot_path)
+                #destroy_snapshot(minor)
+                return False
+
+            if not backup_to_server(mounted_folder_path):
+                umount_snapshot(snapshot_path)
+                destroy_snapshot(minor)
+                return False
+            
         umount_snapshot(snapshot_path)
         destroy_snapshot(minor)
-        return False
-    
-    # Unmount the snapshot after backup
-    umount_snapshot(snapshot_path)
-    destroy_snapshot(minor)
 
     return True
